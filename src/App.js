d195d19a0ce9efde4d863e32b630f03a905b7557
gabriel.grun <gabrielg737@gmail.com>
2020-12-29 14:26:39 -0300

    react-countries

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import Countries from './components/countries/Countries';
+import Header from './components/header/Header';
+
+export default function App() {
+  const [allCountries, setAllCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [filteredPopulation, setFilteredPopulation] = useState(0);
+  const [userFilter, setUserFilter] = useState('');
+
+  useEffect(() => {
+    const getCountries = async () => {
+      const res = await fetch('https://restcountries.eu/rest/v2/all');
+      let allCountries = await res.json();
+
+      allCountries = allCountries.map(
+        ({ name, numericCode, flag, population }) => {
+          return {
+            id: numericCode,
+            name,
+            filterName: name.toLowerCase(),
+            flag,
+            population,
+          };
+        }
+      );
+      setAllCountries(allCountries);
+      setFilteredCountries(Object.assign([], allCountries));
+    };
+
+    getCountries();
+  }, []);
+
+  const calculateTotalPopulationFrom = (countries) => {
+    const totalPopulation = countries.reduce((accum, current) => {
+      return accum + current.population;
+    }, 0);
+
+    return totalPopulation;
+  };
+
+  const handleChangeFilter = (newFilter) => {
+    setUserFilter(newFilter);
+
+    const filterLowerCase = newFilter.toLowerCase();
+
+    const filteredCountries = allCountries.filter((country) => {
+      return country.filterName.includes(filterLowerCase);
+    });
+
+    const filteredPopulation = calculateTotalPopulationFrom(filteredCountries);
+
+    setFilteredCountries(filteredCountries);
+    setFilteredPopulation(filteredPopulation);
+  };
+
+  return (
+    <div className="container">
+      <h1 style={styles.centeredTitle}>React Countries</h1>
+
+      <Header
+        filter={userFilter}
+        countryCount={filteredCountries.length}
+        totalPopulation={filteredPopulation}
+        onChangeFilter={handleChangeFilter}
+      />
+
+      <Countries countries={filteredCountries} />
+    </div>
+  );
+}
+
+const styles = {
+  centeredTitle: {
+    textAlign: 'center',
+  },
+};
